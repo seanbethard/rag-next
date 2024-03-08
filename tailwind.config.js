5f1ce7e71991dc7b4a8f38901eaa8fe53746e0d3
Sean Bethard <sean@seanbethard.net>
2024-03-08 13:19:28 -0600

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,15 +53,15 @@ module.exports = {
         },
         brand: {
           50: '#fff1f1', // Lightest neon, for subtle highlights
-          100: '#ffdfdf', // Very light neon, for background accents
-          200: '#ffc4c4', // Light neon, for buttons and interactive elements
-          300: '#ff9a9a', // Medium neon, for primary UI elements
-          400: '#ff6060', // Vibrant neon, the core brand color
-          500: '#ff2f2f', // Dark neon, for impactful elements
-          600: '#ff2f2f', // Deeper, for hover states or active elements
-          700: '#cc0909', // Darker, for strong contrasts
-          800: '#cc0909', // Even darker, suitable for text or accents
-          900: '#8b1111', // Deepest, for maximum contrast and depth
+          100: '#F2F3F5', // Very light neon, for background accents
+          200: '#E5E7EB', // Light neon, for buttons and interactive elements
+          300: '#D1D5DB', // Medium neon, for primary UI elements
+          400: '#B0B8C1', // Vibrant neon, the core brand color
+          500: '#8E99A4', // Dark neon, for impactful elements
+          600: '#656E7A', // Deeper, for hover states or active elements
+          700: '#4F5966', // Darker, for strong contrasts
+          800: '#3B4553', // Even darker, suitable for text or accents
+          900: '#3B4553', // Deepest, for maximum contrast and depth
         },
         offBlack: {
           950: '#121212',

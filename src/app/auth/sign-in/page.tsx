bba5670a8f7c40c6c55dc1363ed2825cbb7b5968
Sean Bethard <sean@seanbethard.net>
2024-03-08 04:42:20 -0600

    add gatehouse

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -3,9 +3,8 @@ import { redirect } from 'next/navigation'
 import { getSession } from '@/app/_data/user'
 import { Heading } from '@/components/cult/gradient-heading'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-// import { CultIcon } from '@/components/ui/icons'
-
 import { OTPForm } from './oauth-form'
+import {BikerScumGif} from "@/components/ui/icons";
 
 export default async function SignIn() {
   const session = await getSession()
@@ -19,19 +18,19 @@ export default async function SignIn() {
       <section className="h-full w-full">
         <div className=" flex flex-col justify-center items-center mt-16 md:mt-36">
           <div className="flex gap-2">
-            {/*<CultIcon className=" h-16 w-12 md:h-24 md:w-16 mt-1" />*/}
+            <BikerScumGif id="bikerscum" alt="Biker Scum" />
             <div>
-              <Heading size="xxl">ハイパー</Heading>
+              <Heading size="xxl">Biker Scum</Heading>
               <Heading size="sm" variant="secondary" className="-mt-2 md:-mt-4">
-                質問応答
+                Gatekeeper
               </Heading>
             </div>
           </div>
           <div className="flex items-center justify-center pt-12 md:pt-24 ">
             <Card>
               <CardHeader>
-                <CardTitle>入る</CardTitle>
-                <CardDescription>いらしゃい</CardDescription>
+                <CardTitle>Sign in</CardTitle>
+                <CardDescription>Welcome to Biker Scum</CardDescription>
               </CardHeader>
               <CardContent className=" space-y-2">
                 <div className="space-y-2 w-full min-w-[16rem] lg:min-w-[24rem]">

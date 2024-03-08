890b8fb6ff979bb3330c252935e772060fe8acd8
Sean Bethard <sean@seanbethard.net>
2024-03-08 04:24:53 -0600

    base url redirects to sign-in or chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation'
 
 import { getSession } from '@/app/_data/user'
-import { Heading } from '@/components/cult/gradient-heading'
-// import { CultIcon } from '@/components/ui/icons'
 
 export const dynamic = 'force-dynamic'
 
@@ -16,22 +14,4 @@ export default async function SignIn() {
   if (session) {
     return redirect('/chat')
   }
-
-  return (
-    <>
-      <section className="h-full w-full">
-        <div className=" flex flex-col justify-center items-center mt-16 md:mt-36">
-          <div className="flex gap-2">
-            {/*<CultIcon className=" h-16 w-12 md:h-24 md:w-16 mt-1" />*/}
-            <div>
-              <Heading size="xxl">ハイパー</Heading>
-              <Heading size="sm" variant="secondary" className="-mt-2 md:-mt-4">
-                質問応答
-              </Heading>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
 }
